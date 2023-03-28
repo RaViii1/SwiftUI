2017948b2c7b00a1a463a7622b60ade0c51f5092
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-03-28 09:24:44 +0200

    Create marka2.swift

---
+++ Lab5/zad2/marka2.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct marka2: View {
+    var body: some View {
+        VStack{
+            Text("MBW THE iX M60").font(.title)
+            Text("609.000,00 PLN")
+        }
+    }
+}
+
+struct marka2_Previews: PreviewProvider {
+    static var previews: some View {
+        marka2()
+    }
+}
