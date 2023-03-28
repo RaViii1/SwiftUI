19f259892e835f588c29155b9b89a9a4e8810dfa
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-03-28 09:25:03 +0200

    Create marka3.swift

---
+++ Lab5/zad2/marka3.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct marka3: View {
+    var body: some View {
+        VStack{
+            Text("Audi Q8 e-tron").font(.title)
+            Text("345.000,00 PLN")
+        }
+    }
+}
+
+struct marka3_Previews: PreviewProvider {
+    static var previews: some View {
+        marka3()
+    }
+}
