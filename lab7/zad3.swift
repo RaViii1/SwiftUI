c25d7011a1033a6aebaeaba1fb22c6dde64d56f0
RaVi <77927691+RaViii1@users.noreply.github.com>
2023-06-10 17:47:06 +0200

    Update zad3.swift

--- lab7/zad3.swift
+++ lab7/zad3.swift
@@ -16,8 +16,7 @@ struct AuthorSearchView: View {
         Book(author: "J.K. Rowling", title: "Harry Potter and the Chamber of Secrets", availableCopies: 1),
         Book(author:"George Orwell", title:"1984",availableCopies :5)
         
-        //dodaj pozostałe książki tutaj
-        
+        // Add more books here
     ]
     
     var body : some View{
@@ -52,21 +51,18 @@ struct AuthorSearchView: View {
                                                                 .foregroundColor(Color.gray)
 
                                                         }
-
-                                                
-                                            
-                                        }
-                                        
+                                    }
                                     
-                                }}
                                 
+                            }}
                             
-                            .navigationBarTitle(Text("Książki"))
-                            }
-                            
-                        }}
                         
-                    
+                        .navigationBarTitle(Text("Książki"))
+                        }
+                        
+                    }
+}
+
 struct BookDetailView:View{
     
     let bookDetailAuthor:String
@@ -88,26 +84,57 @@ struct BookDetailView:View{
                     Text("\(book.availableCopies) egzemplarzy dostępnych")
                         .foregroundColor(Color.gray)
 
-                
                 }
-                
-                
-                
+
             }
             
             
         }.padding(.horizontal,10).padding(.top,5)
-        
-
-
-        
-        
-        
-        
-        
+ 
     }
+
+}
+
+struct ContentView: View {
     
+    @State private var authorName = ""
     
-    
-    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                TextField("Nazwisko autora", text: $authorName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                NavigationLink(destination:
+                                AuthorSearchView(searchTerm:self.authorName)){
+                                    
+                                    Text("Szukaj książek")
+                                        .fontWeight(.semibold)
+                                        .frame(minWidth: 0, maxWidth: .infinity)
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .background(Color.blue.opacity(0.8))
+                                        .cornerRadius(40)                     
+                            }.disabled(authorName.isEmpty || authorName.trimmingCharacters(in:.whitespaces).isEmpty )
+ 
+                        Spacer()
+
+               
+            }.padding()
+            
+
+
+            
+        }.navigationTitle(Text("Wyszukiwanie książek"))
+
+}
+}
+
+struct ContentView_Previews : PreviewProvider {
+
+static var previews : some View {
+
+ContentView()
+
+}
 }
